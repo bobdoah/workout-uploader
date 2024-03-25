fa93d4982bcc097451ed25053ba54c1b341da18d
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2024-03-25 21:14:44 +0000

    feat: add strava uploader

---
+++ strava/uploader.py
@@ -0,0 +1,42 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.server.received = parse_qs(self.path.split("?", 1)[1])
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(
+            b"""<html>
+            <head><title>Strava auth code received</title></head>
+            <body><p>This window can be closed</p></body>
+            </html>"""
+        )
+
+
+class TokenHandler(HTTPServer):
+    def __init__(self, port=65500):
+        self.received = None
+        HTTPServer.__init__(self, ("localhost", port), handler)
+
+
+def get_client_config():
+    with open("strava.toml", "rb") as f:
+        data = tomllib.load(f)
+    return data["client_id"], data["client_secret"]
+
+
+def get_authorized_client():
+    client_id, client_secret = get_client_config()
+    th = TokenHandler()
+    authorize_url = stravalib.client.authorization_url(
+        client_id=client_id,
+        redirect_url=f"http://{th.server_address}",
+        scope=["profile:read_all", "activity:read_all", "activity:write"],
+    )
+    webbrowser.open_new_tab(authorize_url)
+    th.handle_request()
+    token = stravalib.client.exchange_code_for_token(
+        client_id=client_id, client_secret=client_secret, code=th.received["code"]
+    )
