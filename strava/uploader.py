66b600ba309a0b325791f24d3ca373fffb753530
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2024-04-02 21:38:02 +0100

    feat: set gear on upload

--- strava/uploader.py
+++ strava/uploader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import os
 import argparse
+import os
 
 from strava_client import get_authorized_client
 
@@ -14,13 +14,20 @@ def main():
     args = p.parse_args()
 
     client = get_authorized_client()
+    bikes = {b.name: b.id for b in client.get_athlete().bikes}
+    gear_id = ""
+    if args.bike:
+        gear_id = bikes[args.bike]
     for file in args.activities:
         _, ext = os.path.splitext(file.name)
         upload = client.upload_activity(
             file, ext[1:], commute=args.commute, activity_type=args.activity_type
         )
         activity = upload.wait()
-        print(f"http://strava.com/activities/{activity.id:d}")
+        print(f"uploaded: http://strava.com/activities/{activity.id:d}")
+        if gear_id:
+            client.update_activity(activity_id=activity.id, gear_id=gear_id)
+            print(f"set gear to {args.bike}")
 
 
 if __name__ == "__main__":
